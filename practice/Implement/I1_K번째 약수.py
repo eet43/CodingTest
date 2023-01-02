d8a5a264f156f0d3ff950864801cab08abd892d2
Daisy <59008469+eet43@users.noreply.github.com>
2023-01-02 19:01:15 +0900

    Update I1_K번째 약수.py

--- practice/Implement/I1_K번째 약수.py
+++ practice/Implement/I1_K번째 약수.py
@@ -1,1 +1,19 @@
-print
+import sys
+n,k = map(int, input().split())
+
+def getValue(n):
+  value = []
+  for i in range(2, n+1):
+    if n%i==0:
+      value.append(i)
+      
+  return value
+
+
+result = getValue(n)
+result.sort()
+if(len(result) < k):
+  print(-1)
+else:
+  print(result)
+  print(result[k-1])
